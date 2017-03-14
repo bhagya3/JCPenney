9f58d13698d5c32ff5a843aa5ebfec7470ef9c32
Bhagyshri Nivdunge <vibha.nivdunge@gmail.com>
2017-03-14 09:36:28 +0530

    added product list page

---
+++ src/pages/productlist/productlist.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { GetProductList } from '../../providers/get-product-list';
+
+
+/*
+  Generated class for the Departments page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-productlist',
+  templateUrl: 'productlist.html',
+  providers: [GetProductList]
+})
+export class ProductlistPage {
+  public products: any;
+  department : any;
+  defaultUrl : any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public getProdList: GetProductList) { 
+    this.department = navParams.get('department');
+    this.defaultUrl = navParams.get('defaultUrl');
+    this.loadProductList();
+  }
+
+loadProductList(){
+  this.getProdList.load(this.department)
+  .then(data => {
+    this.products = [];
+    console.log(data.groups.length);
+    for(let i = 0 ;i<data.groups.length;i++){
+      console.log(data.groups[i]);
+      this.products = this.products.concat(data.groups[i].categories);
+    }
+    console.log("Hihihihi")
+    console.log(this.products);
+    // console.log(data);
+  });
+}
+  ionViewDidLoad() {
+    //console.log('ionViewDidLoad DepartmentsPage');
+
+  }
+
+}
